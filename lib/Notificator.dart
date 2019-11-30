6b60a5a0d4a28d0999c985bd692a8110d1ec895c
danil.khandys <danil.khandys@xpage.ru>
2019-11-30 16:01:36 +0500

    1232

---
+++ lib/Notificator.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Notificator extends StatelessWidget{
+  @override
+  Widget build(ctx){
+    return MaterialApp(
+      title: "К&Б - оповещение водителей",
+      theme: ThemeData(
+        primaryColor: const Color(0xFF323232)
+      ),
+      // home: 
+    );
+  }
+}
